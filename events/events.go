8f28cf33266ee94fc97d0d6b6208561c1df1f08d
Austin Clements <austin@google.com>
2024-07-03 14:27:43 -0400

    events: add software events

--- events/events.go
+++ events/events.go
@@ -47,6 +47,7 @@ func (e eventBasic) String() string {
 }
 
 var (
+	// Hardware events
 	EventCPUCycles       = eventBasic{"cpu-cycles", unix.PERF_TYPE_HARDWARE, unix.PERF_COUNT_HW_CPU_CYCLES}
 	EventInstructions    = eventBasic{"instructions", unix.PERF_TYPE_HARDWARE, unix.PERF_COUNT_HW_INSTRUCTIONS}
 	EventCacheReferences = eventBasic{"cache-references", unix.PERF_TYPE_HARDWARE, unix.PERF_COUNT_HW_CACHE_REFERENCES}
@@ -54,4 +55,19 @@ var (
 	EventBranches        = eventBasic{"branches", unix.PERF_TYPE_HARDWARE, unix.PERF_COUNT_HW_BRANCH_INSTRUCTIONS}
 	EventBranchesMisses  = eventBasic{"branch-misses", unix.PERF_TYPE_HARDWARE, unix.PERF_COUNT_HW_BRANCH_MISSES}
 	EventBusCycles       = eventBasic{"bus-cycles", unix.PERF_TYPE_HARDWARE, unix.PERF_COUNT_HW_BUS_CYCLES}
+)
+
+var (
+	// Software events
+	EventCPUClock        = eventBasic{"cpu-clock", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_CPU_CLOCK}
+	EventTaskClock       = eventBasic{"task-clock", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_TASK_CLOCK}
+	EventPageFaults      = eventBasic{"page-faults", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_PAGE_FAULTS}
+	EventContextSwitches = eventBasic{"context-switches", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_CONTEXT_SWITCHES}
+	EventCPUMigrations   = eventBasic{"cpu-migrations", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_CPU_MIGRATIONS}
+	EventMajorFaults     = eventBasic{"minor-faults", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_PAGE_FAULTS_MIN}
+	EventMinorFaults     = eventBasic{"major-faults", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_PAGE_FAULTS_MAJ}
+	EventAlignmentFaults = eventBasic{"alignment-faults", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_ALIGNMENT_FAULTS}
+	EventEmulationFaults = eventBasic{"emulation-faults", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_EMULATION_FAULTS}
+	EventDummy           = eventBasic{"dummy", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_DUMMY}
+	EventBPFOutput       = eventBasic{"bpf-output", unix.PERF_TYPE_SOFTWARE, unix.PERF_COUNT_SW_BPF_OUTPUT}
 )
